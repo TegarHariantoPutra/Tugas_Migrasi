390ce6f6539010f5f77f61e79a436b9f43ae75f7
TegarHariantoPutra <tegar_harianto_27rpl@student.smktelkom-mlg.sch.id>
2020-01-23 12:28:03 +0700

    perubahan ketiga

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,11 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+    
 });
+
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
+Route::get('user', 'UserController@getAuthenticatedUser')->middleware('jwt.verify');
+
+
